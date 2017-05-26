880dbeaa8dc4b9c7cd0275222c0012f0c7c74da8
razerno <razerrno@gmail.com>
2017-05-26 20:58:56 +0100

    Added pause and resume to music playback.

--- commands/play.js
+++ commands/play.js
@@ -5,21 +5,43 @@ exports.run = (client, message, params) => {
   let guildId = message.member.guild.id;
   if (client.voiceConnections.has(guildId)) {
     let connection = client.voiceConnections.get(guildId);
-    const info = ytdl.getInfo(params[0])
-      .then(info => {
-        const stream = ytdl.downloadFromInfo(info, {filter: 'audioonly'});
-        if (connection.dispatcher) {
-          connection.dispatcher.end('stop');
+    if (params.length > 0) {
+      const info = ytdl.getInfo(params[0])
+        .then(info => {
+          const stream = ytdl.downloadFromInfo(info, {filter: 'audioonly'});
+          if (connection.dispatcher) {
+            connection.dispatcher.end('stop');
+          }
+          player.play(guildId, info);
+          const dispatcher = connection.playStream(stream, player.streamOptions);
+          message.channel.send(`Playing: *${info.title}*`);
+          addListener(message.channel, guildId, connection, dispatcher);
+        })
+        .catch(err => {
+          console.log(err);
+          message.channel.send("That's not a valid youtube link!");
+        });
+    } else {
+      if (connection.dispatcher) {
+        if (connection.dispatcher.paused) {
+          message.channel.send('Resuming music playback.');
+          connection.dispatcher.resume();
+        } else {
+          message.channel.send("I'm already playing!");
         }
-        player.play(guildId, info);
-        let dispatcher = connection.playStream(stream, player.streamOptions);
-        message.channel.send(`Playing: *${info.title}*`);
-        addListener(message.channel, guildId, connection, dispatcher);
-      })
-      .catch(err => {
-        console.log(err);
-        message.channel.send("That's not a valid youtube link!");
-      });
+      } else {
+        const info = player.next(guildId);
+        if (info) {
+          const stream = ytdl.downloadFromInfo(info, {filter: 'audioonly'});
+          player.play(guildId, info);
+          const dispatcher = connection.playStream(stream, player.streamOptions);
+          message.channel.send(`Playing: *${info.title}*`);
+          addListener(message.channel, guildId, connection, dispatcher);
+        } else {
+          message.channel.send("There's nothing in queue.");
+        }
+      }
+    }
   } else {
     message.channel.send("I'm not in a voice channel. Use !join to have me join the voice channel you're in.");
   }
